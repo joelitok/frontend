86be1fc87542a08789826d998dc196f3330ef855
joelitok <joelnkouatchet@gmail.com>
2022-04-28 14:33:59 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auths/login/login.component';
+import { RegisterComponent } from './auths/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component:  LoginComponent},
+  { path: 'register', component:  RegisterComponent},
+  { path: 'dashboard', component:  DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
